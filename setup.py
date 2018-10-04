0571ec0667a45daee9f5005995ecc0bf1600985b
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-10-04 11:48:53 +0200

    missing comma...

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ CLASSIFIERS = [
     'Topic :: Scientific/Engineering :: Bio-Informatics'
 ]
 INSTALL_REQUIRES = [
-    'pybel>=0.12.1'
+    'pybel>=0.12.1',
     'bio2bel>=0.1.5',
     'sqlalchemy',
     'click',
