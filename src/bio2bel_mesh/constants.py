4122520de460d98aa735d454ba5f25a25ed2ab5e
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-07-26 15:09:52 +0200

    Update data to 2018

--- src/bio2bel_mesh/constants.py
+++ src/bio2bel_mesh/constants.py
@@ -11,12 +11,12 @@ VERSION = '0.2.1-dev'
 MODULE_NAME = 'mesh'
 DATA_DIR = get_data_dir(MODULE_NAME)
 
-YEAR = '2017'
+YEAR = '2018'
 
-DESCRIPTOR_URL = f'ftp://nlmpubs.nlm.nih.gov/online/mesh/.xmlmesh/desc{YEAR}.gz'
+DESCRIPTOR_URL = f'ftp://nlmpubs.nlm.nih.gov/online/mesh/{YEAR}/xmlmesh/desc{YEAR}.gz'
 DESCRIPTOR_PATH = os.path.join(DATA_DIR, f'desc{YEAR}.gz')
 DESCRIPTOR_JSON_PATH = os.path.join(DATA_DIR, f'desc{YEAR}.json')
 
-SUPPLEMENT_URL = f'ftp://nlmpubs.nlm.nih.gov/online/mesh/.xmlmesh/supp{YEAR}.gz'
+SUPPLEMENT_URL = f'ftp://nlmpubs.nlm.nih.gov/online/mesh/{YEAR}/xmlmesh/supp{YEAR}.gz'
 SUPPLEMENT_PATH = os.path.join(DATA_DIR, f'supp{YEAR}.gz')
 SUPPLEMENT_JSON_PATH = os.path.join(DATA_DIR, f'supp{YEAR}.json')
